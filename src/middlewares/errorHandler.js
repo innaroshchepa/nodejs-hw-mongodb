c997acb992123be569fb1d22a9da5db58f7c8883
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2024-06-21 17:29:31 +0200

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -9,10 +9,8 @@ export const errorHandler = (err, req, res) => {
     });
     return;
   }
-
   res.status(500).json({
-    status: 500,
     message: 'Something went wrong',
-    error: err.message,
+    data: err.message,
   });
 };
