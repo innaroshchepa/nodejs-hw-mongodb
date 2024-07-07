b0e9f7eae8d81c8d9dcfbbf21a0f13304c094c85
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2024-07-07 12:13:02 +0200

    create hw7

--- src/server.js
+++ src/server.js
@@ -7,15 +7,13 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import rootRouter from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -23,7 +21,11 @@ export const setupServer = () => {
       },
     }),
   );
+  app.use('/api-docs', swaggerDocs());
+  app.use(cors());
+  app.use(cookieParser());
 
+  app.use(express.json());
   app.use(rootRouter);
 
   app.use('*', notFoundHandler);
